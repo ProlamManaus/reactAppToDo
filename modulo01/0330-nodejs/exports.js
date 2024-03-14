a1f61c4d71d2d780930e47e19762f4f27a98c353
Prolam Manaus <121299247+ProlamManaus@users.noreply.github.com>
2024-03-14 17:32:43 -0400

    Delete modulo01 directory

--- modulo01/0330-nodejs/exports.js
+++
@@ -1,1 +0,0 @@
-module.exports = "Prolam: module.exports"
--- modulo01/0357-timers/setInterval.js
+++
@@ -1,9 +0,0 @@
-// setInterval ira rodar um função N vezes
-
-const timeOut = 1000
-console.log('start!')
-const checking = () => console.log('checking!')
-
-setInterval(checking, timeOut)
-console.log('after 1 2 3 seconds.')
-
